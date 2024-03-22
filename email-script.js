16aad42929dbb9449d9e58ec25bea6c8ce4d54bd
BuildTools <unconfigured@null.spigotmc.org>
2024-03-21 19:50:52 -0700

    pretty much all done :)

--- email-script.js
+++ email-script.js
@@ -1,11 +1,71 @@
 // email-script.js
 
-// Array containing the subject, sender, and content of the inbox emails
+// Array containing the subject, sender, and content of the emails
 var emails = [
     {
         subject: "Big Things",
         sender: "bigman@gmail.com",
-        content: "This is a test",
+        content: "I got some big things cooking up, and you're gonna want to get in on this, believe you me!",
+    },
+
+    {
+        subject: "Obvious Scam",
+        sender: "known_scammer95@hotmail.com",
+        content: "CLick thIs liNK t0 LosE ALL ur mUney1! shady_link.xyz",
+    },
+
+    {
+        subject: "Pls be my friend",
+        sender: "loner01@gmail.com",
+        content: "Look, I'll be honest. I have many friends. But I need more....",
+    },
+
+    {
+        subject: "RE: Concerning the elves",
+        sender: "gandalf@wizard.net",
+        content: "Hey bud, it's the big G. Just getting back to you bout them elves. I know they can be a handful, but trust me dude they're cool. Hope you and the missus are doing well. Peace. B)",
+    },
+
+    {
+        subject: "GIVE US OUR MONEY!!!!",
+        sender: "da_mob@gabagool.com",
+        content: "Ok wise guy, you had ya chance. You think you're funny, huh?! Let's see if ya still laughin' after Lil Gambino breaks ya thumbs!! -Sent from my iPhone",
+    },
+
+    {
+        subject: "JUST REACHING OUT :)",
+        sender: "GRANGRAN56@yahoo.com",
+        content: "HEY THERE SWEETIE, JUST THOUGHT I'D DROP YOU A LINE ON MY NEW COMPUTER BOOK. THAT'S RIGHT, GRAN GRAN IS ONLINE :) SMILEY FACE. I WANTED TO SEND SOME COOKIES, BUT I'M NOT QUITE SURE HOW TO OVER THE EMAILS. ANY WAYS HOPE YOU'RE NOT SMOKING STILL. LOVE GRAM GRAM",
+    },
+
+    {
+        subject: "Purchase Confirmed",
+        sender: "doritosupport@fritolay.com",
+        content: "Your order is on its way! Order receipt: 100 x Cool Ranch Doritos Party Size",
+    },
+
+    {
+        subject: "CLICK HERE OR ELSE",
+        sender: "oreo@cookie.web",
+        content: "BUY OREOS! BUY OREOS! BUY OREOS! BUY OREOS! BUY OREOS! BUY OREOS! BUY OREOS! BUY OREOS! BUY OREOS! BUY OREOS! BUY OREOS! BUY OREOS! BUY OREOS!",
+    },
+
+    {
+        subject: "Pants on sale now!",
+        sender: "oldnavy@clothes.com",
+        content: "We just got our newest lines of PANTS for the summer, but hurry on over now or else it'll be too late :'(",
+    },
+
+    {
+        subject: "GOKU NEEDS YOUR ENERGY",
+        sender: "piccolo@kami.net",
+        content: "RIP Toriyama",
+    },
+
+    {
+        subject: "Nothing wierd I promise",
+        sender: "drew@fakemail.com",
+        content: "Hey pal, there's nothing really going on in this email. Thanks for clicking on this / reading the code. Have a blessed day :)",
     },
 ];
 
@@ -35,9 +95,13 @@ function openInbox() {
     // Sets inbox display to block
     document.getElementById('inbox-section').style.display = 'block';
 
-    // Hides open email section
+    // Gets open-email-section by Id and stores the variable
     var openEmailSection = document.getElementById('open-email-section');
-    openEmailSection.classList.toggle('d-none');
+
+    // Checks if display is not set to none, and then toggles it on
+    if (!openEmailSection.classList.contains('d-none')){
+        openEmailSection.classList.toggle('d-none');
+    }
 }
 
 
@@ -55,8 +119,8 @@ function updateOpenEmailContent(index) {
     var emailBodyTextarea = document.getElementById('email-body');
 
     // Set values for input fields
-    emailSubjectInput.value = email.subject;
-    emailSenderInput.value = email.sender;
+    emailSubjectInput.value = 'Subject: ' + email.subject;
+    emailSenderInput.value = 'From: ' + email.sender;
     emailBodyTextarea.value = email.content;
 
     // Check if input fields are not readonly and then add readonly attribute
@@ -105,8 +169,8 @@ function checkScreenSize() {
     // Check if the media query matches (screen size is large)
     if (mediaQuery.matches) {
 
-        // Set the inbox section to block
-        document.getElementById('inbox-section').style.display = 'block';
+        // Call openInbox() to set inbox display to block and open email to d-none
+        openInbox();
     }
 }
 
@@ -134,7 +198,7 @@ function handleSliderChange() {
     document.getElementById('open-email-section').style.backgroundColor = 'hsl(' + hue + ', 50%, 55%)';
             
     // Set the background color of the sidebar
-    document.getElementById('sidebar').style.backgroundColor = 'hsl(' + hue + ', 50%, 55%)';
+    document.getElementById('sidebar').style.backgroundColor = 'hsl(' + hue + ', 50%, 40%)';
             
     // Set the background color of the inbox section
     document.getElementById('inbox-section').style.backgroundColor = 'hsl(' + hue + ', 50%, 55%)';
